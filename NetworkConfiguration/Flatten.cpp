7f40f2c604681becd23a577d09f9e84ee536fdee
Luka Križan <bulletformyporcupine@gmail.com>
2017-05-01 21:37:01 +0200

    Added flattening layer
    - used as a bridge between convolution-like operations to fully connected
      layers

---
+++ NetworkConfiguration/Flatten.cpp
@@ -0,0 +1,37 @@
+#include "Flatten.h"
+
+namespace NetworkConfiguration {
+
+Flatten::Flatten(tensorflow::Scope & scope, const tensorflow::Input & previousLayerOutput, const Shape & previousLayerOutputShape) :
+  NonParameterizedLayer(scope)
+{
+  // check if flattening is possible (input rank 4)
+  if (previousLayerOutputShape.size() != 4)
+  {
+    throw std::logic_error("Flattening can only be performed on rank 4 tensor inputs.");
+  }
+
+  // set name
+  m_Index = ++s_TotalNumber;
+  std::string outputName = s_LayerName + std::to_string(m_Index) + "_out";
+
+  const std::vector<tensorflow::int64> & previousLayerShapeValues = previousLayerOutputShape.data();
+  const unsigned int numExamples = previousLayerShapeValues[0];
+  const unsigned int height = previousLayerShapeValues[1];
+  const unsigned int width = previousLayerShapeValues[2];
+  const unsigned int numFiltersInput = previousLayerShapeValues[3];
+  const unsigned int numElements = height * width * numFiltersInput;
+  m_OutputShape = Shape({ numExamples, numElements });
+  m_Output = tensorflow::ops::Reshape(m_Scope.WithOpName(outputName), previousLayerOutput,
+    tensorflow::Input(tensorflow::Input::Initializer({ -1, numElements })));
+}
+
+}   // namespace NetworkConfiguration
+
+
+// register class in factory
+namespace {
+  using namespace NetworkConfiguration;
+  LayerCreator ctor = [](LayerBaseParams & params) {return new Flatten(params);};
+  bool dummy = LayerFactory::instance().registerClass("Flatten", ctor);
+}
---
+++ NetworkConfiguration/Flatten.h
@@ -0,0 +1,27 @@
+#ifndef Flatten_h
+#define Flatten_h
+
+#include "Layer.h"
+
+namespace NetworkConfiguration {
+
+// flattens every learning example to a vector
+class Flatten : public NonParameterizedLayer
+{
+private:
+  // used for placeholder symbolic names
+  int m_Index;
+  static int s_TotalNumber;
+  static const std::string s_LayerName;
+
+public:
+  Flatten(tensorflow::Scope &scope, const tensorflow::Input &previousLayerOutput, const Shape& previousLayerOutputShape);
+  Flatten(LayerBaseParams & params) : Flatten(params.scope_, params.previousLayerOutput_, params.previousLayerOutputShape_) {};
+};
+
+int Flatten::s_TotalNumber = 0;
+const std::string Flatten::s_LayerName = "Flatten";
+
+}	// namespace NetworkConfiguration
+
+#endif
