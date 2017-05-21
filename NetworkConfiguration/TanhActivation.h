05a2e8d2783a1fe6be62b69cf1323cfa702db54d
Luka Križan <bulletformyporcupine@gmail.com>
2017-05-21 18:54:01 +0200

    Return of the SoftmaxActivation (but without backward pass)

--- NetworkConfiguration/TanhActivation.h
+++ NetworkConfiguration/TanhActivation.h
@@ -5,20 +5,20 @@
 
 namespace NetworkConfiguration {
 
-  class TanhActivation : public NonParameterizedLayer
-  {
-  private:
-    static int s_TotalNumber;
-    static const std::string s_LayerName;
+class TanhActivation : public NonParameterizedLayer
+{
+private:
+  static int s_TotalNumber;
+  static const std::string s_LayerName;
 
-  public:
-    TanhActivation(tensorflow::Scope &scope, const tensorflow::Input &previousLayerOutput, const Shape& previousLayerOutputShape);
-    TanhActivation(LayerBaseParams & params) : TanhActivation(params.scope_, params.previousLayerOutput_, params.previousLayerOutputShape_) {};
-    tensorflow::Output backwardInputs(const tensorflow::Input & previousInputsGradient) override;
-  };
+public:
+  TanhActivation(tensorflow::Scope &scope, const tensorflow::Input &previousLayerOutput, const Shape& previousLayerOutputShape);
+  TanhActivation(LayerBaseParams & params) : TanhActivation(params.scope_, params.previousLayerOutput_, params.previousLayerOutputShape_) {};
+  tensorflow::Output backwardInputs(const tensorflow::Input & previousInputsGradient) override;
+};
 
-  int TanhActivation::s_TotalNumber = 0;
-  const std::string TanhActivation::s_LayerName = "TanhActivation";
+int TanhActivation::s_TotalNumber = 0;
+const std::string TanhActivation::s_LayerName = "TanhActivation";
 
 
 }   // namespace NetworkConfiguration
