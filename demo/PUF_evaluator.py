2b8b3934cd3bb2dc6ba32c223f4c7380f9c2030a
Luka Križan <bulletformyporcupine@gmail.com>
2017-05-27 18:10:18 +0200

    Added PUF evaluation python script

---
+++ demo/PUF_evaluator.py
@@ -0,0 +1,39 @@
+from model_loader import *
+import os
+import numpy as np
+import tensorflow as tf
+import pdb
+from sklearn.metrics import f1_score
+
+if __name__ == "__main__":
+    DATA_DIR = "../example_datasets/PUF/"
+    INPUTS_FILE = "f_1x64_100000.txt"
+    LABELS_FILE = "r_1x64_100000.txt"
+    # read inputs
+    test_x = []
+    fp = open(os.path.join(DATA_DIR, INPUTS_FILE), "r")
+    for line in fp:
+        line = line.split()
+        test_x.append([int(x) for x in line])
+    test_x = np.array(test_x)
+    fp.close()
+    # read labels
+    test_y = []
+    fp = open(os.path.join(DATA_DIR, LABELS_FILE), "r")
+    for line in fp:
+        line = line.split()
+        # change -1 into zeros for easier evaluation
+        test_y.append([int(x) > 0 for x in line])
+    fp.close()
+    test_y = np.array(test_y)
+    # load trained model
+    loader = ModelLoader("./PUF/")
+    feed_dict = loader.feed_dict
+    tf.import_graph_def(loader.graph_def, name="")
+    with tf.Session() as sess:
+        feed_dict["inputs:0"] = test_x
+        output = sess.run(["TanhActivation1_out:0"], feed_dict=feed_dict)
+        class_output = output[0] > 0
+        result = class_output == test_y
+        print("Test accuracy: ", np.mean(result))
+        print("F1 score: ", f1_score(test_y, class_output))
