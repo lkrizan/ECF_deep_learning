ba776abc2095d757f88b0ae50678dbe0b3b868a5
Luka Križan <bulletformyporcupine@gmail.com>
2017-06-04 23:06:30 +0200

    Saving per milestones
    - trained model is now saved in a uniquely named folder (date + curr time)
    - model is periodically (can be configured), in the last generation and
      when fitness reaches terminal value

--- DeepModelTraining/ModelSaveOp.h
+++ DeepModelTraining/ModelSaveOp.h
@@ -4,19 +4,21 @@
 #include <ECF/ECF.h>
 #include "ModelEvalOp.h"
 #include <fstream>
+#include <windows.h>
 
 class ModelSaveOp : public Operator
 {
+protected:
   bool m_SaveModel = false;
   std::string m_ModelExportPath;
   ModelEvalOpP m_EvaluationOp;
   SelBestOpP m_SelBestOp;
   std::ofstream m_OutputStream;
-
+  uint m_SaveMilestone;
+  double m_TerminatingFitness;
+  uint m_TerminatingGeneration;
   // helper function for final evaluation of saved model
   double getAverageTrainingError(IndividualP individual);
-  // model is saved after the last generation is completed
-  uint m_TerminatingGeneration;
 public:
   ModelSaveOp() : m_SelBestOp(new SelBestOp) {}
   void registerParameters(StateP state) override;
